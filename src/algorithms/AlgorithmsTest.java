2eaf33fe7f17b54f00b0aac45a009450e38697ab
Sam Coco <Samuel.HenriAC@outlook.com>
2019-10-01 18:38:58 +0100

    Further testing added for Quick and Selection sort

--- src/algorithms/AlgorithmsTest.java
+++ src/algorithms/AlgorithmsTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 public class AlgorithmsTest {
     private Double[] unsortedArray = {4000d, -535.5, 54d, 32d, 3.14159, 3.14159};
     private Double[] expectedSort = {-535.5, 3.14159, 3.14159, 32d, 54d, 4000d};
+    private Double[] unsortedArray2 = {0d, 0d, 0d, 0d, 0d};
+    private Double[] expectedSort2 = {0d, 0d, 0d, 0d, 0d};
+    private Double[] emptyArray = {};
 
     @Test
     public void bubbleSort(){
@@ -22,10 +25,14 @@ public class AlgorithmsTest {
     @Test
     public void quickSort(){
         assertArrayEquals(Algorithms.quickSort(unsortedArray, 0, 5), expectedSort);
+        assertArrayEquals(Algorithms.quickSort(unsortedArray2, 0, 4), expectedSort2);
+        assertNull(Algorithms.quickSort(emptyArray,0, 0));
     }
 
     @Test
     public void selectionSort(){
         assertArrayEquals(Algorithms.selectionSort(unsortedArray), expectedSort);
+        assertArrayEquals(Algorithms.selectionSort(unsortedArray2), expectedSort2);
+        assertNull(Algorithms.selectionSort(emptyArray));
     }
 }
